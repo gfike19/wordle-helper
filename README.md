cf4ad661cac9721b60dcbcf14265588e1dabfd5a
gfike19 <gfike19@gmail.com>
2024-06-02 19:57:47 -0500

    got starter word and main menu functions working

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Personal app created to assist with playing Wordle by providing possible first guesses, and hints. 
 
-## 6/2/2024 ## added gitignore, updated words left file
+## 6/2/2024 ## 
+added gitignore, updated words left file
+got starter word and main menu functions working
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import secrets
+
 def StarterWord():
-    f = open("words-left.txt", "r")
-    wordsLeft = f.readlines()
+    getAnother = "y"
+    while getAnother == "y":
+        f = open("words-left.txt", "r+")
+        wordsLeft = f.readlines()
+        f.close()
+        randomWord = secrets.choice(wordsLeft)
+        print(randomWord)
+        getAnother = input("Get another word (y/n)? ").lower()
+    MainMenu()
+
+# def RemoveWord():
+            # how to clear text in file
+        # f.truncate(0)
 
 def MainMenu():
-    mainMenuChoice = int(input("""Welcome to Wordle Helper!\n1) Get a starter word\n2) Get possible guesses
-    \n3) Remove word from list of possibles\n4)Exit program"""))
+    mainMenuChoice = int(input("""
+    Welcome to Wordle Helper!
+    1) Get a starter word
+    2) Get possible guesses
+    3) Remove word from list of possibles
+    4)Exit program\n"""))
 
     if mainMenuChoice == 1:
         StarterWord()
@@ -12,4 +29,7 @@ def MainMenu():
         Guesses()
     if mainMenuChoice == 3:
         RemoveWord()
-    exit()
+    else:
+        exit()
+
+MainMenu()
